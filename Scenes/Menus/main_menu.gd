94fa74c3e39a487000708b0dc84fe15468c7f31a
Makerspace-Durham <durhammaker1@outlook.com>
2024-11-05 12:59:23 -0500

    Added more functionality to game manager

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -1,14 +1,12 @@
 extends Control
 
-@onready var game_manager = get_tree().root.get_node("GameManager")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _on_start_pressed() -> void:
-	game_manager.change_scene(Global.TEST_LEVEL)
+	Global.game_manager.change_scene(Global.TEST_LEVEL)
 
 
 func _on_quit_pressed():
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -5,4 +5,9 @@ extends Node
 #Levels
 var TEST_LEVEL="res://Scenes/Levels/test.tscn"
 
+#Soundtrack
+var PIXELATED_DREAMS="res://Assets/Audio/Music/Pixelated Dreams.mp3"
+var PIXELATED_JOURNEY="res://Assets/Audio/Music/Pixelated Journey.mp3"
+
 #Misc
+@onready var game_manager = get_tree().root.get_node("GameManager")
