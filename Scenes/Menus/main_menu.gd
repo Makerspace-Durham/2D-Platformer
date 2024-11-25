c89aa604611bc918c976856b3715e9f42b508032
jiarguet <jordanargueta@absolutezerostudios.com>
2024-11-24 20:49:56 -0500

    accidentally overrode something and had to revert it.... whoopsie

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -1,18 +1,13 @@
 extends Control
-var scene = preload("res://Scenes/Sandbox/jiarguet_sandbox.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+    pass
 
 
 func _on_start_pressed() -> void:
-	Global.game_manager.change_scene(Global.TEST_LEVEL)
+    Global.game_manager.change_scene(Global.TEST_LEVEL)
 
 
 func _on_quit_pressed():
     get_tree().quit()
-
-
-func _on_start_pressed() -> void:
-    get_tree().root.add_child(scene)
