5cd7de5c7e5b7bacf653886e6e22ef57ed84ab7e
Makerspace-Durham <durhammaker1@outlook.com>
2025-05-20 11:25:42 -0400

    Updated menu options and added assets

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -8,6 +8,8 @@ func _ready():
 func _on_start_pressed() -> void:
 	Global.game_manager.change_scene(Global.TEST_LEVEL)
 
+func _on_jungle_pressed() -> void:
+	Global.game_manager.change_scene(Global.JUNGLE_DEMO)
 
 func _on_quit_pressed():
 	get_tree().quit()
