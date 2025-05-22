f053d9c5be815894c2b9bd62ffba86f425e803d2
Makerspace-Durham <durhammaker1@outlook.com>
2025-05-22 16:11:48 -0400

    cleanup

--- Scenes/Player/Actions/idle.gd
+++ Scenes/Player/Actions/idle.gd
@@ -7,32 +7,32 @@ class_name IdleState
 # this prevents the player from sliding when transitioning 
 # from a moving state
 func enter_state(player, input, delta):
-    remove_player_velocity(player)
-    control(player, input, delta)
+	remove_player_velocity(player)
+	control(player, input, delta)
 
 
 # Control function: Handles the core logic and animator
 func control(player, input, delta):
-    abstraction(player, input, delta)
-    presentation(player)
+	abstraction(player, input, delta)
+	presentation(player)
 
 # Abstraction function: Handles the core logic
 func abstraction(player, input, delta):
-    if not player.is_on_floor():
-        # in cases where the floor is pulled out from under the player
-        player.exit_state_and_transition(player, input, delta, player.fall)
+	if not player.is_on_floor():
+		# in cases where the floor is pulled out from under the player
+		player.exit_state_and_transition(player, input, delta, player.fall)
 
-    if input.input_direction != 0:
-        player.exit_state_and_transition(player, input, delta, player.run)
-    
-    if input.quick_time_actions.has("jump"):
-        player.exit_state_and_transition(player, input, delta, player.jump)
-    
+	if input.input_direction != 0:
+		player.exit_state_and_transition(player, input, delta, player.run)
+	
+	if input.quick_time_actions.has("jump"):
+		player.exit_state_and_transition(player, input, delta, player.jump)
+	
 
 # Presentation function: Controls the animator
 func presentation(player):
-    player.get_node("AnimatedSprite2D").play("idle")
+	player.get_node("AnimatedSprite2D").play("idle")
 
 
 func remove_player_velocity(player):
-    player.velocity = Vector2(0, 0)
+	player.velocity = Vector2(0, 0)
